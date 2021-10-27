c10a22f9512b0e2bdaa074cb498f14c5eb708c14
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-10-27 17:36:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,5 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
 git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
-
 git clone https://github.com/kenzok8/openwrt-packages.git ./package/openwrt-packages
 git clone https://github.com/kenzok8/small.git ./package/small
