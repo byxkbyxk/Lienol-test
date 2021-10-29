3b41d0f86566b0bb4958a2f3673a0bb4ca0efdf9
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-10-29 14:34:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
-git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
 # https://github.com/messense/aliyundrive-webdav
