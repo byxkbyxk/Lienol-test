9b56bb1e788d69b2042e6abe93ac0ad6ccf43346
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-11-03 13:52:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@ sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt 
 
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
-git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
 git clone https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
